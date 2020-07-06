13eed52a654cd966981e8611ee43aa410cb71645
gitandroids <okyayonur1@gmail.com>
2020-07-06 14:38:09 +0300

    Work on multiple documents,add and retrive them.

--- app/src/main/java/gitandroid/com/firestoreexample/MainActivity.java
+++ app/src/main/java/gitandroid/com/firestoreexample/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -19,6 +20,8 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
@@ -34,8 +37,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextDescription;
     private TextView textViewData;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();    // Referance for firestore database.
+    private CollectionReference notebookRef = db.collection("Notebook");
     private DocumentReference noteRef = db.document("Notebook/My First Note");
     //private ListenerRegistration noteListener;  if we want to detach the listener manually..
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,28 +55,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() { // just put "this" to detach it on activity stop. We could also do detach onStop manually -- noteListener = noteRef.addSnapshotListener...  and remove listener onStop()..
+        notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                 if (e != null) {
-                    Toast.makeText(MainActivity.this, "Could not load firebase exception!", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, e.toString());
                     return;
-                } else {
-                    if (documentSnapshot.exists()) {
-/*                        String title = documentSnapshot.getString(KEY_TITLE);
-                        String description = documentSnapshot.getString(KEY_DESCRIPTION);
-                        textViewData.setText("Title:" + title + "\n" + "Description:" + description);*/
-                        Note note = documentSnapshot.toObject(Note.class);   // variable names should match!
-                        String title = note.getTitle();
-                        String description = note.getDescription();
-                        textViewData.setText("Title:" + title + "\n" + "Description:" + description);
-                    } else {
-                        Toast.makeText(MainActivity.this, "Document does not exist!", Toast.LENGTH_SHORT).show();
-                        textViewData.setText("");
-                    }
-
                 }
+                String data = "";
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    Note note = documentSnapshot.toObject(Note.class);
+                    note.setDocumentId(documentSnapshot.getId());
+                    String documentId = note.getDocumentId();
+                    String title = note.getTitle();
+                    String description = note.getDescription();
+                    data += "ID: "+documentId
+                            + "\nTitle: " + title + "\nDescription: " + description + "\n\n";
+                   // notebookRef.document(documentId).update   To get the id of the document and work on it..
+                }
+                textViewData.setText(data);
             }
         });
     }
@@ -82,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
         // noteListener.remove();  to remove listener manually
     }
 
-    public void saveNote(View v) {     // we have to set it public because we set it in .xml and it should take View in it.
+    public void addNote(View v) {     // we have to set it public because we set it in .xml and it should take View in it.
         String title = editTextTitle.getText().toString();
         String description = editTextDescription.getText().toString();
         // we need to use container to send the variables . We cannot just pass them directly.
@@ -95,65 +96,27 @@ public class MainActivity extends AppCompatActivity {
         Note note = new Note(title, description);  // we could use MAP object instead of our Java Object
         //db.document("Notebook/My First Note");  shorter version to name collection and document but the other one is better to use
         // We could create sub collections      db.collection("Notebook").document("My First Note").collection()set(note) . .
-        noteRef.set(note)  // db.collection("Notebook").document("My First Note").set(note)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
+        notebookRef.add(note);
+    }
+
+
+    public void loadNotes(View v) {
+        notebookRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
-                    }
-                });
 
-    }
-
-    public void updateDescription(View view) {  // This updates description but also all the document so the title would be null.
-        String description = editTextDescription.getText().toString();
-        //Map<String, Object> note = new HashMap<>();
-        //note.put(KEY_DESCRIPTION, description);
-        //noteRef.set(note, SetOptions.merge()); this will update the field , but if the document doesn't exist this also create one..
-        noteRef.update(KEY_DESCRIPTION, description); // we could use Map as above instead. This wont create the document , if does not exist.
-    }
-
-    public void deleteDescription(View v) {
-        //Map<String, Object> note = new HashMap<>();
-        //note.put(KEY_DESCRIPTION, FieldValue.delete());
-        //noteRef.update(note);
-        noteRef.update(KEY_DESCRIPTION, FieldValue.delete());
-    }
-
-    public void deleteNote(View v) {
-        noteRef.delete();
-    }
-
-    public void loadNote(View v) {
-        noteRef.get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-/*                          String title = documentSnapshot.getString(KEY_TITLE);
-                            String description = documentSnapshot.getString(KEY_DESCRIPTION);*/
-                            Note note = documentSnapshot.toObject(Note.class);   // variable names should match!
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) { // we have querysnapshot now instead of documentsnapshot.
+                        String data = "";
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {   // QueryDocumentSnapshot always exist. No need to check it. DocumentSnapshot is the superclass of it.
+                            Note note = documentSnapshot.toObject(Note.class);
+                            note.setDocumentId(documentSnapshot.getId());
+                            String documentId = note.getDocumentId();
                             String title = note.getTitle();
                             String description = note.getDescription();
-                            //Map<String, Object> note = documentSnapshot.getData();
-                            textViewData.setText("Title:" + title + "\n" + "Description:" + description);
-                        } else {
-                            Toast.makeText(MainActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
+                            data += "ID: "+documentId
+                                   + "\nTitle: " + title + "\nDescription: " + description + "\n\n";
                         }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
+                        textViewData.setText(data);
                     }
                 });
     }
--- app/src/main/java/gitandroid/com/firestoreexample/Note.java
+++ app/src/main/java/gitandroid/com/firestoreexample/Note.java
@@ -1,9 +1,11 @@
 package gitandroid.com.firestoreexample;
 
+import com.google.firebase.firestore.Exclude;
+
 public class Note {
     private String title;
     private String description;
-
+    private String documentId;
     public Note() {  // Firestore always needs Empty constructor . Otherwise it will crash..
         //public no-arg constructor needed
     }
@@ -16,5 +18,13 @@ public class Note {
     }
     public String getDescription() {
         return description;
+    }
+    @Exclude      // We dont want this id to appear in firestore so that we @exclude it. // There is an id anyways . It would be redundant
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
     }
 }
